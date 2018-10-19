3d3985b27262c73253e58d6851c070b3d958da43
AltusN <altusnel@gmail.com>
2018-10-19 01:16:42 +0000

    added template

--- flask_app.py
+++ flask_app.py
@@ -1,14 +1,17 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
 @app.route("/")
 def hello_world():
-    return "Peanuts are fun"
+    return render_template("main_page.html")
 
+
+#Leave this for the A7 tracker
 @app.route("/location", methods=("POST",))
 def submit_location_data():
     if request.method == "POST":
