a4faff09594ee5205ab89e227e513438e693d430
AltusN <altusnel@gmail.com>
2018-10-19 00:44:00 +0000

    Initial Commit

---
+++ flask_app.py
@@ -0,0 +1,20 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Peanuts are fun"
+
+@app.route("/location", methods=("POST",))
+def submit_location_data():
+    if request.method == "POST":
+        lat = request.form.get("lat")
+        lng = request.form.get("lng")
+    else:
+        data = "error"
+    return str(data)
+
