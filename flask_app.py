02e6092a9dc6a784c0a7ea36049f9842269f4fd8
AltusN <altusnel@gmail.com>
2018-10-19 01:40:19 +0000

    get and post

--- flask_app.py
+++ flask_app.py
@@ -1,14 +1,20 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for, redirect
 
 app = Flask(__name__)
-app.config["DEBUG"] = True
+app.config["DEBUG"] = False
 
-@app.route("/")
-def hello_world():
-    return render_template("main_page.html")
+comments = []
+
+@app.route("/", methods=("GET","POST"))
+def index():
+    if request.method == "GET":
+        return render_template("main_page.html", comments=comments)
+
+    comments.append(request.form["contents"])
+    return redirect(url_for("index"))
 
 
 #Leave this for the A7 tracker
